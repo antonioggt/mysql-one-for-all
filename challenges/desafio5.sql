4ccdac86308578b6d80f8444f160a01b63f0bc23
antonioggt <antonioggt85@gmail.com>
2023-03-28 09:28:27 -0300

    [feat] requisito 6

--- challenges/desafio5.sql
+++ challenges/desafio5.sql
@@ -1,7 +1,7 @@
 SELECT
     (s.song_name) AS cancao, COUNT(p.song_id) AS reproducoes
 FROM
-songs AS s INNER JOIN playback_history AS p ON p.song_id = song.song_id
+songs AS s INNER JOIN playback_history AS p ON p.song_id = songs.song_id
 GROUP BY p.song_name
 ORDER BY reproducoes DESC , cancao ASC
 LIMIT 2;
---
+++ challenges/desafio6.sql
@@ -0,0 +1,9 @@
+SELECT
+    MIN(s.price) AS faturamento_minimo,
+    MAX(s.price) AS faturamento_maximo,
+    ROUND(AVG(s.price), 2) AS faturamento_medio,
+    SUM(p.price) AS faturamento_total
+FROM
+    subscription_type AS s
+        INNER JOIN
+    users AS u ON u.subscription_type_id = s.subscription_type_id;
