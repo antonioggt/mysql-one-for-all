c68c917739f912b3966a234a02215ee2fe7b46f7
antonioggt <antonioggt85@gmail.com>
2023-03-27 23:35:32 -0300

    [fix] SQL syntax errors

--- challenges/desafio2.sql
+++ challenges/desafio2.sql
@@ -1,6 +1,8 @@
+USE 
+
 SELECT
-    COUNT(DISTINCT songs.id) AS cancoes,
-    COUNT(DISTINCT artists.id) AS artistas,
-    COUNT(DISTINCT albuns.id) AS albuns
+    COUNT(DISTINCT songs.song_id) AS cancoes,
+    COUNT(DISTINCT artists.artist_id) AS artistas,
+    COUNT(DISTINCT albuns.album_id) AS albuns
 FROM
     albuns INNER JOIN songs INNER JOIN artists;
